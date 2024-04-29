c4150476d4363d3f4b1fe5b863ccf33c3e534da4
Jairo Ortega <jl.ortega01072001@gmail.com>
2024-04-29 23:21:50 +0800

    first try learning js

---
+++ index.js
@@ -0,0 +1,55 @@
+console.log(`Hello JS!`);
+console.log(`More Improvement!!!`);
+
+// window.alert(`Alerttt!`);
+
+// document.getElementById("h1").textContent = "Hello World!";
+// document.getElementById("p").textContent = "Try something new!";
+
+// let age = 25;
+
+// console.log(`You're ${age} years old.`);
+
+// let username;
+
+// document.getElementById("mySubmit").onclick = function() {
+//     username = document.getElementById("username").value;
+//     document.getElementById("myH1").textContent = `Welcome, ${username}`;
+// }
+
+// let age = window.prompt("Age: ");
+
+// age = Number(age);
+// age+=1;
+
+// console.log(age);
+
+// TODO: COUNTER PROGRAM 
+
+// const decrease = document.getElementById('decreaseBtn');
+// const reset = document.getElementById('resetBtn');
+// const increase = document.getElementById('increaseBtn');
+// const num = document.getElementById('countNum');
+// let count = 0;
+
+// increase.onclick = function(){
+//     count++;
+//     num.textContent = count;
+// }
+
+// decrease.onclick = function(){
+//     count--;
+//     num.textContent = count;
+// }
+
+// reset.onclick = function(){
+//     count = 0;
+//     num.textContent = count;
+// }
+
+
+const min = 100;
+const max = 200;
+
+let randomNum = Math.floor(Math.random() * (max - min)) + min;
+console.log(randomNum);
