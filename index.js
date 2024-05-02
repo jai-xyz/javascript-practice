50efb2356e4d2c93317b5a1d9ad71bd6475961e0
Jairo Ortega <jl.ortega01072001@gmail.com>
2024-05-02 15:21:18 +0800

    still learning js

--- index.js
+++ index.js
@@ -66,3 +66,98 @@ console.log(`More Improvement!!!`);
 //       const displayResult = document.getElementById("displayResult");
 //       displayResult.textContent = sum;
 //     }
+
+
+    //  document.getElementById("myButton").onclick = function(){
+
+    //    let age = document.getElementById("age").value;
+    //    age = Number(age);
+
+    //  display = age >= 18 ? "You're an adult" : "You're an minor";
+
+    //     document.getElementById("display").innerHTML = display;
+    // }
+
+
+// Method Chaining
+    // let username = window.prompt("Enter your username: ");
+
+    // username = username.trim().charAt(0).toUpperCase() + username.trim().slice(1).toLowerCase(); 
+
+    // console.log(username);
+
+    // const min = 1;
+    // const max = 100;
+    // const answer = Math.floor(Math.random() * (max - min + 1)) + min;
+
+    // // console.log(answer);
+
+    // let attempts = 0;
+    // let guess;
+    // let running = true;
+
+    // while(running){
+    //     guess = window.prompt(`Guess a number between ${min} - ${max}`);
+    //     guess = Number(guess);
+    //     // console.log(typeof guess, guess);
+
+    //     if(isNaN(guess)){
+    //         window.alert("Please enter a valid number")
+    //     }
+    //     else if (guess < min || guess > max){
+    //         window.alert("Please enter a valid number")
+    //     } 
+    //     else {
+    //         attempts++;
+    //         if (guess < answer){
+    //             window.alert("Your answer is too low! Try again!");
+    //         } 
+    //         else if (guess > answer){
+    //             window.alert("Your answer is too high! Try again!");
+    //         }
+    //         else if (guess){
+    //             window.alert(`You get it! The answer is ${answer}. Congratulations!`);
+    //             running = false;
+    //         }
+    //     }
+
+    // }
+
+
+    // function isLegal(age){
+    //     let message = age >= 18 ? "your age is legal" : "your are an minor";
+    //      return message;
+    // }
+
+    // console.log(isLegal(18));
+
+    // const userInput = document.getElementById("userInput");
+    // const toFarenheit = document.getElementById("toFarenheit");
+    // const toCelsius = document.getElementById("toCelsius");
+    // const displayResult = document.getElementById("displayResult");
+    // let temp;
+
+    // function convert(){
+    //     if(toFarenheit.checked){
+    //         temp = Number(userInput.value);
+    //         temp = temp * 9 / 5 + 32;
+    //         displayResult.textContent = temp.toFixed(1) + "°F";
+    //     }
+    //     else if (toCelsius.checked){
+    //         temp = Number(userInput.value);
+    //         temp = (temp - 32) * (5 / 9);
+    //         displayResult.textContent = temp.toFixed(1) + "°C";
+    //     }
+    //     else {
+    //         displayResult.style.color="red";
+    //         displayResult.textContent = "Please select a unit";
+    //     }
+    // }
+
+    // TODO: enhanced for loop
+    
+let sports = ["volleyball", "basketball", "soccer", "baseball"];
+
+for (let sport of sports){
+    console.log(sport);
+}
